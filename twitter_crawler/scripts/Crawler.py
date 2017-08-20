640c5dd047c3de8b8178c5901726f12c57a548e0
kazuma1989 <funifuni.1204@gmail.com>
2017-08-20 15:12:13 +0900

    Crawler のリファクタリング
    - Ctrl + C で中断できるようになった

--- twitter_crawler/scripts/Crawler.py
+++ twitter_crawler/scripts/Crawler.py
@@ -1,4 +1,4 @@
-from threading import Timer, get_ident
+from threading import Timer
 
 from elasticsearch import Elasticsearch
 from twitter import Twitter, TwitterStream, OAuth
@@ -29,9 +29,27 @@ class Crawler:
 
         self.__es = Elasticsearch(["http://elastic:changeme@elasticsearch:9200"])
 
-    def __refresh_trend_list(self):
-        response = self.__twitter.trends.place(_id=self.__woeid)[0]["trends"]
-        self.__trend_list = [trend["name"] for trend in response]
+    def __push_to_index(self, tweet):
+        # 取得上限超えた時にくるLimit Jsonは無視
+        if "limit" in tweet:
+            return tweet
+
+        # ツイート内にキーワードが見つからないときは無視
+        # 大文字・小文字の違いや、日本語表記・アルファベット表記などの揺らぎで起きることがある
+        trend = self.__find_matching_trend(self.__trend_list, tweet["text"])
+        if not trend:
+            return
+
+        doc = {
+            "track": trend,
+            "text": tweet["text"],
+            "created_at": tweet["created_at"],
+            # "geo": tweet["geo"],
+            # "place": tweet["place"],
+        }
+        self.__es.index(index="testindex", doc_type="tweet", body=doc)
+
+        return doc
 
     def __push_tweets_tracked_by_trend(self):
         track = ",".join(self.__trend_list)
@@ -39,34 +57,32 @@ class Crawler:
 
         i = 0
         for tweet in tweet_iter:
-            if self.__current_thread_ident != get_ident():
+            trend_list_updated = track != ",".join(self.__trend_list)
+            if trend_list_updated:
+                print("New trend comes")
                 return
 
-            # 取得上限超えた時にくるLimit Jsonは無視
-            if "limit" in tweet:
-                print(tweet)
-                continue
-
-            trend = self.__find_matching_trend(self.__trend_list, tweet["text"])
-            if not trend:
-                continue
-
-            doc = {
-                "track": trend,
-                "text": tweet["text"],
-                "created_at": tweet["created_at"],
-                # "geo": tweet["geo"],
-                # "place": tweet["place"],
-            }
+            doc = self.__push_to_index(tweet)
 
             i += 1
             print(i, doc)
 
-            self.__es.index(index="testindex", doc_type="tweet", body=doc)
+    def __interval_refresh_trend_list(self):
+        response = self.__twitter.trends.place(_id=self.__woeid)[0]["trends"]
+        self.__trend_list = [trend["name"] for trend in response]
+
+        print("Refreshed")
+
+        self.__timer = Timer(300, self.__interval_refresh_trend_list)
+        self.__timer.start()
 
     def run(self):
-        self.__current_thread_ident = get_ident()
-        Timer(300, self.run).start()
+        self.__interval_refresh_trend_list()
 
-        self.__refresh_trend_list()
-        self.__push_tweets_tracked_by_trend()
+        try:
+            while True:
+                self.__push_tweets_tracked_by_trend()
+
+        except KeyboardInterrupt:
+            self.__timer.cancel()
+            exit()
