94c94097951bf9756c587fbe8c65df05320b25c3
kazuma1989 <funifuni.1204@gmail.com>
2017-08-20 13:54:33 +0900

    複数トレンドキーワードで検索できるようにした
    - Stream は一つしか接続できないらしいので複数スレッドで実行するのは断念
    - Ctrl + C で中断できるが、スレッドが残ってしまうのか python プロセス自体が終わらない

--- twitter_crawler/scripts/Crawler.py
+++ twitter_crawler/scripts/Crawler.py
@@ -1,7 +1,19 @@
+from threading import Timer, get_ident
+
 from elasticsearch import Elasticsearch
 from twitter import Twitter, TwitterStream, OAuth
 
 class Crawler:
+
+    @staticmethod
+    def __find_matching_trend(trend_list, tweet_text):
+        for trend in trend_list:
+            if trend in tweet_text:
+                return trend
+
+        # Nothing found
+        return None
+
     def __init__(self, oauth_info):
         oauth = OAuth(**oauth_info)
         self.__twitter = Twitter(auth=oauth)
@@ -21,29 +33,40 @@ class Crawler:
         response = self.__twitter.trends.place(_id=self.__woeid)[0]["trends"]
         self.__trend_list = [trend["name"] for trend in response]
 
-    def __push_tweets_tracked_by(self, track):
+    def __push_tweets_tracked_by_trend(self):
+        track = ",".join(self.__trend_list)
         tweet_iter = self.__twitter_stream.statuses.filter(track=track)
 
         i = 0
         for tweet in tweet_iter:
+            if self.__current_thread_ident != get_ident():
+                return
+
             # 取得上限超えた時にくるLimit Jsonは無視
             if "limit" in tweet:
                 print(tweet)
                 continue
 
+            trend = self.__find_matching_trend(self.__trend_list, tweet["text"])
+            if not trend:
+                continue
+
             doc = {
-                "track": track,
+                "track": trend,
                 "text": tweet["text"],
                 "created_at": tweet["created_at"],
+                # "geo": tweet["geo"],
+                # "place": tweet["place"],
             }
 
             i += 1
-            print(track, i, doc)
+            print(i, doc)
 
             self.__es.index(index="testindex", doc_type="tweet", body=doc)
 
     def run(self):
-        self.__refresh_trend_list()
+        self.__current_thread_ident = get_ident()
+        Timer(300, self.run).start()
 
-        trend = self.__trend_list[0]
-        self.__push_tweets_tracked_by(trend)
+        self.__refresh_trend_list()
+        self.__push_tweets_tracked_by_trend()
